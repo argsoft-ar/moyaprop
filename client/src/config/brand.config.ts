dee57338daeac6c83b507fc2eba6ba3d4a1f523f
Rodrigo Podoba <rgonzalopodoba@gmail.com>
2026-09-18 20:24:44 -0300

    importacion de la foto desde cloudinary

--- client/src/config/brand.config.ts
+++ client/src/config/brand.config.ts
@@ -52,7 +52,7 @@ export const brandConfig: BrandConfig = {
   agent: {
     name: 'Carlos Moya',
     title: 'Martillero y Corredor Inmobiliario',
-    photoUrl: '/carlos.webp'
+    photoUrl: 'https://res.cloudinary.com/dry6jcgfn/image/upload/v1789773827/carlos_paileh.webp'
   },
 
   license: {
